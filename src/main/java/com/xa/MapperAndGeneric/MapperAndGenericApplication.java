544dcfb7ed2a626fd53b00032c2e84fcee0d6fb9
XAbdulfayz <usa.xamidov@gmail.com>
2023-09-17 13:15:42 +0500

    initial

---
+++ src/main/java/com/xa/MapperAndGeneric/MapperAndGenericApplication.java
@@ -0,0 +1,13 @@
+package com.xa.MapperAndGeneric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperAndGenericApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperAndGenericApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xa/MapperAndGeneric/MapperAndGenericApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xa.MapperAndGeneric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapperAndGenericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
