5e02299e7c6719de262708f6ca3010b25c174da7
XAbdulfayz <usa.xamidov@gmail.com>
2023-09-24 14:51:36 +0500

    p

--- src/main/java/com/xa/MapperAndGeneric/controller/PostController.java
+++ src/main/java/com/xa/MapperAndGeneric/controller/PostController.java
@@ -28,7 +28,7 @@ public class PostController {
     }
     @GetMapping("get/{id}")
     public PostGetDto get(@PathVariable Long id){
-        return service.get(id);
+        return service.get(id); // Sevinch
     }
 
 
